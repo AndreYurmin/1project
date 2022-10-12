251f56287451984797d5fcb679650e38af38e442
AndreYurmin <a.jurmin@yandex.ru>
2022-10-12 07:53:09 +0700

    Добавлен метод printAllExpenses

--- Cont.java
+++ Cont.java
@@ -1,5 +1,10 @@
-public static conv {
+public plass conv {
 
+  void printAllExpenses() {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        }
+    }
 
 }
 
